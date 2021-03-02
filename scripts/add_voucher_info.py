3f58adf8607254b4757581f7e818d01e69157654
Sahil <sahil.holacash.mx>
2021-03-02 13:03:17 +0530

    updated the scripts

--- scripts/add_voucher_info.py
+++ scripts/add_voucher_info.py
@@ -2,14 +2,16 @@ from openpyxl import load_workbook
 from api.models import MSReceivedVouchers
 
 wb = load_workbook("voucher_info.xlsx")
+MSReceivedVouchers.objects.all().delete()
 for ws in wb.worksheets:
     for i in range(1, ws.max_row + 1):
-        voucher_no = ws.cell(column=1, row=i).value
+        voucher_no = str(ws.cell(column=1, row=i).value)
         pin = str(ws.cell(column=2, row=i).value)
         amount = ws.cell(column=3, row=i).value
         if voucher_no is not None and pin is not None:
+            voucher_no = voucher_no.replace("'", "")
             pin = pin.replace("'", "")
-            ms_voucher_info, created = MSReceivedVouchers.objects.get_or_create(
+            ms_voucher_info = MSReceivedVouchers.objects.create(
                 voucher_no=voucher_no, pin=pin, amount=amount
             )
 
--- scripts/get_pending_vouchers.py
+++ scripts/get_pending_vouchers.py
@@ -2,13 +2,23 @@ from api.models import MSReceivedVouchers, MSOrderVouchers
 
 
 ms_received_vouchers_qs = MSReceivedVouchers.objects.all()
+pending_bal = 0
 for ms_received_voucher in ms_received_vouchers_qs:
     ms_voucher_qs = MSOrderVouchers.objects.filter(
         voucher_no=ms_received_voucher.voucher_no, pin=ms_received_voucher.pin
     )
     if ms_voucher_qs.exists():
         ms_voucher = ms_voucher_qs.first()
-        if ms_voucher.amount < ms_received_voucher.amount:
+        diff = ms_received_voucher.amount - ms_voucher.amount
+        if diff > 0:
             print(
-                f"Voucher no: {ms_voucher.voucher_no} and Voucher Pin {ms_voucher.pin} with balance left { ms_received_voucher.amount - ms_voucher.amount} original amount: { ms_received_voucher.amount}  "
+                f"Voucher no: {ms_voucher.voucher_no} and Voucher Pin {ms_voucher.pin} with balance left: {diff} original amount: { ms_received_voucher.amount}  "
             )
+            pending_bal = pending_bal + diff
+    else:
+        print(
+            f"Unused voucher: Voucher no: {ms_received_voucher.voucher_no} and Voucher Pin {ms_received_voucher.pin} amount: { ms_received_voucher.amount}  "
+        )
+        pending_bal = pending_bal + ms_received_voucher.amount
+
+print(pending_bal)
